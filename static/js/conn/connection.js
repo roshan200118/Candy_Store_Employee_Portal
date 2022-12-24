7aac94e310448cfa102a06af2d87d368f4b4a354
Roshan Nanthapalan <roshan200118@gmail.com>
2022-12-24 14:19:13 -0500

    Update DB dump and connection

--- static/js/conn/connection.js
+++ static/js/conn/connection.js
@@ -3,10 +3,10 @@ const mysql = require('mysql');
 function newConn()
 {
     let conn = mysql.createConnection({
-        host:'localhost',
-        user: 'root',
-        password:'password',
-        database:'candy_retail'
+        host:'us-cdbr-east-06.cleardb.net',
+        user: 'b81cfff364960c',
+        password:'9d994542',
+        database:'heroku_ffb9511d068313f'
     });
     return conn;
 }
