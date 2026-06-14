e3171ced1dc30bed33f5746e433c20ae394b783c
Roshan Nanthapalan <roshan200118@gmail.com>
2026-06-14 17:53:50 -0400

    Security Fix

--- db/connection.js
+++ db/connection.js
@@ -1,19 +1,14 @@
 const mysql = require('mysql2');
 
-function newConn()
-{
-    let conn = mysql.createConnection({
-        host: process.env.DB_HOST || 'lg7j30weuqckmw07.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-        user: process.env.DB_USER || 'kive3rcqiam2uci0',
-        password: process.env.DB_PASSWORD || 'ajsz1u4maufeqx8u',
-        database: process.env.DB_NAME || 't650mhy2tt432hkp'
+function newConn() {
+    const conn = mysql.createConnection({
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME
     });
-    // let conn = mysql.createConnection({
-    //     host:'localhost',
-    //     user: 'root',
-    //     password:'password',
-    //     database:'candy_retail'
-    // });
+
     return conn;
 }
+
 module.exports = newConn;
